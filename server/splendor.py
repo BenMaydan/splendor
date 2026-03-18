3e880421fa188d221666bfaab2be43ac71eda138
Ben Maydan <maydanb@gmail.com>
2026-03-17 22:45:42 -0700

    fixed python 2 bugs

--- server/splendor.py
+++ server/splendor.py
@@ -6,6 +6,7 @@ import random
 import time
 import json
 import os
+import platform
 import sys
 
 app = Flask(__name__)
@@ -114,6 +115,8 @@ class GameManager(object):
         return {'id': pid, 'uuid': uuid}
 
     def start_game(self):
+        if self.game.num_players < 2:
+            return {'error': 'Need at least 2 players to start'}
         if self.game.start_game():
             self.has_changed()
             self.started = True
@@ -125,7 +128,7 @@ def game_manager_from_dict(obj):
     self = GameManager(obj['uuid'])
     self.starter = obj['starter']
     self.game = game_from_dict(obj['game'])
-    for k, v in obj['changed'].iteritems():
+    for k, v in obj['changed'].items():
         self.changed[int(k)] = v
     self.chats = obj['chats']
     self.ended = obj['ended']
@@ -196,7 +199,7 @@ def start_game(game, starter):
 def suggest_game():
     global game_map, words
     n = len(words)
-    idx = random.choice(xrange(n))
+    idx = random.choice(range(n))
     start = idx
     while words[idx] in game_map:
         idx = (idx + 1) % n
@@ -264,7 +267,7 @@ def act(game, action, target):
 @json_response
 def list_games():
     delete_games = []
-    for k, v in game_map.iteritems():
+    for k, v in game_map.items():
         if not v.started and time.time() - v.created > 600:
             delete_games.append(k)
         elif v.started and time.time() - v.created > 24*60*60:
@@ -327,7 +330,7 @@ def save_and_exit(number, frame):
     global game_map
 
     games = {}
-    for k, v in game_map.iteritems():
+    for k, v in game_map.items():
         games[k] = v.private_dict()
     with open('server/save.json', 'w') as f:
         f.write(json.dumps(games))
@@ -341,10 +344,15 @@ if __name__ == '__main__':
     try:
         with open('server/save.json') as f:
             save = json.loads(f.read())
-            for k, v in save.iteritems():
+            for k, v in save.items():
                 game_map[k] = game_manager_from_dict(v)
     except IOError:
         pass
 
-    signal.signal(signal.SIGHUP, save_and_exit)
+    if platform.system() != 'Windows':
+        signal.signal(signal.SIGHUP, save_and_exit)
+    # These usually work on both, but Windows only supports a limited set
+    signal.signal(signal.SIGINT, save_and_exit)  # Handles Ctrl+C
+    signal.signal(signal.SIGTERM, save_and_exit) # Handles termination
+
     app.run(host='127.0.0.1', port=8000, threaded=True)
